a3a9ba17a1289b41298929b508bc4d06983e5df6
MenfTours <menftourss@gmail.com>
2024-03-23 00:51:10 +0200

    'first'

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    // $("body *:not('#loader, #loader img')").css("visibility",'hidden')
+    // setTimeout(() => {
+    //     $("body *:not('#loader, #loader img')").css("visibility",'visible')
+    //     $("body #loader").css("visibility",'hidden')
+    // }, 5000);
+
+
+    
+})
