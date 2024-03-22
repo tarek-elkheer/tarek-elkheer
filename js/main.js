ebab0ce77bdb48944dc18058a151451912a2a73d
MenfTours <menftourss@gmail.com>
2024-03-23 01:12:03 +0200

    'first'

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 $(function(){
-    // $("body *:not('#loader, #loader img')").css("visibility",'hidden')
-    // setTimeout(() => {
-    //     $("body *:not('#loader, #loader img')").css("visibility",'visible')
-    //     $("body #loader").css("visibility",'hidden')
-    // }, 5000);
-
-
-    
+    $("body *:not('#loader, #loader img')").css("visibility",'hidden')
+    setTimeout(() => {
+        $("body *:not('#loader, #loader img')").css("visibility",'visible')
+        $("body #loader").css("visibility",'hidden')
+    }, 5000);
 })
