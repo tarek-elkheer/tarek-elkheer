66d3c18d730b1707e24b77a3d03ed8ca1ea1512f
MenfTours <menftourss@gmail.com>
2024-03-23 10:12:18 +0200

    'first'

--- js/main.js
+++ js/main.js
@@ -4,4 +4,23 @@ $(function(){
         $("body *:not('#loader, #loader img')").css("visibility",'visible')
         $("body #loader").css("visibility",'hidden')
     }, 5000);
+
+
+    $("#more").on("click",function(){
+        $(this).css('display','block')
+        $("#options ").slideToggle(100)
+        $("#options").css("flex-direction","column")
+ 
+
+    })
+
+
+    // ScROLLER
+    let el = document.querySelector(".scroller");
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop;
+    el.style.width = `${(scrollTop / height) * 100}%`;
+    });
 })
